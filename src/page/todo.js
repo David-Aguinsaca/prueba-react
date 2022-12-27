7cfc1fb55c82cd482deb5b7669019b1f17a53361
David-Aguinsaca <davinsaca@yahoo.com>
2022-12-27 11:52:53 -0500

    CON-6 add create gif

--- src/page/todo.js
+++ src/page/todo.js
@@ -8,6 +8,8 @@ import axios from 'axios';
 
 function Todo() {
 
+  const baseURL = process.env.REACT_API_GIF
+
   let valueInitial = {
     url: "",
     author_id: 1014
@@ -19,12 +21,13 @@ function Todo() {
   const [error, setError] = useState(null);
 
   const handleChange = (event) => {
+    //console.log(event.target.value);
     setFormValue({ ...formValue, [event.target.name]: event.target.value });
   }
 
   useEffect(() => {
     axios
-      .get("https://iyelrnlkoq7ra5mnxg5cobbkta0uubul.lambda-url.us-east-1.on.aws/?author_id="+ valueInitial.author_id)
+      .get("https://iyelrnlkoq7ra5mnxg5cobbkta0uubul.lambda-url.us-east-1.on.aws/?author_id=1014")
       .then((response) => {
         console.log(response.data);
         setGifList(response.data);
@@ -32,16 +35,39 @@ function Todo() {
       .catch((error) => setError(error));
   }, []);
 
+  const createGif = () => {
+
+    if (formValue.value === '') {
+      alert('No puede estar vacio');
+      return false;
+    } else if (formValue.url.trim() === '') {
+      alert('Debe ingresar un valor en el campo');
+      return false;
+    }
+
+    axios.post("https://iyelrnlkoq7ra5mnxg5cobbkta0uubul.lambda-url.us-east-1.on.aws/", formValue)
+        .then((response) => {
+            console.log(response.data);
+            setGifList([...gifList, response.data]);
+            setFormValue(valueInitial);
+        });
+  }
+
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '10px', padding: '5px' }}>
       <Card style={{ width: '25rem' }} className="text-center">
         <Card.Header>
           <h3>Gif Galery</h3>
-          <TopBar></TopBar>
+          <TopBar
+            value={formValue.url}
+            name="url"
+            handleChange={handleChange}
+            createGif={createGif}
+          ></TopBar>
         </Card.Header>
         <Card.Body>
-          <GifCardList gifList = {gifList}></GifCardList>
+          <GifCardList gifList={gifList}></GifCardList>
         </Card.Body>
       </Card>
     </div>
