3c190eed311f2070dd1854686e41f05c1f822b2f
David-Aguinsaca <davinsaca@yahoo.com>
2022-12-27 10:03:16 -0500

    CON-2 style component button

---
+++ src/component/atoms/button.js
@@ -0,0 +1,11 @@
+
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+
+function ButtonPersonalized() {
+    return (
+        <Button variant="outline-secondary">Secondary</Button>
+    );
+  }
+  
+  export default ButtonPersonalized;
